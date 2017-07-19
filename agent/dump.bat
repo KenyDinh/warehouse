173d8c02a2dd3522bbb6995fee0dc5922d7f5f4f
KenyDinh <kenydinh@gmail.com>
2017-07-19 21:44:52 +0700

    remove unncessary file

--- agent/dump.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c%%a%%b)
-For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a%%b)
-set name=backup/warehouse_%mydate%_%mytime%_backup.sql
-mysqldump -uroot -pkenydinh2609 warehouse > %name%
